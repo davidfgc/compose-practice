8344d20b4b574652c386562693d69cac57e25dfa
David Gonzalez <david@solucionespruna.com>
2024-08-30 15:15:16 -0500

    build: migrate to kotlin 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.jetbrains.kotlin.android)
+  alias(libs.plugins.compose.compiler)
 
   id("kotlin-android")
   id("kotlin-kapt")
@@ -39,9 +40,6 @@ android {
   }
   buildFeatures {
     compose = true
-  }
-  composeOptions {
-    kotlinCompilerExtensionVersion = "1.5.1"
   }
   packaging {
     resources {
